aa0fdc7de14be6f06bdefbc588a57189bb6c2b79
adhirajbhattacharya <41481263+adhirajbhattacharya@users.noreply.github.com>
2026-09-14 17:34:14 +0530

    Add: ransom-note - submission-1

---
+++ Data Structures & Algorithms/ransom-note/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            count.put(c, 1 + count.getOrDefault(c, 0));
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            count.put(c, -1 + count.getOrDefault(c, 0));
+            if (count.get(c) < 0) return false;
+        }
+
+        return true;
+    }
+}
